d4591d51728ce584d19d8467f2900ef844c47555
YannickChamberland <142918103+YannickChamberland@users.noreply.github.com>
2025-10-20 14:13:03 -0400

    ajout du v for et ajout dans le tableau

--- script.js
+++ script.js
@@ -5,27 +5,43 @@ data: function (){
             {
                 titre: "Paysage",
                 type: "Audio et Animation 3D",
-                logiciel: "Maya, Reaper et Max"
+                logiciel: "Maya, Reaper et Max",
+                description : "",
+                cadre : "",
+                mention : ""
             },
             {
                 titre: "Personnage",
                 type: "3D",
-                logiciel: "Maya"
+                logiciel: "Maya",
+                description : "",
+                cadre : "",
+                mention : ""
             },
             {
                 titre: "Paysage abstrait",
                 type: "3D",
-                logiciel: "Maya"
+                logiciel: "Maya",
+                description : "",
+                cadre : "",
+                mention : "",
+                equipe : ""
             },
             {
                 titre: "media_131_T_MULTI_chamberland_yannick",
                 type: "Audio",
-                logiciel: "Reaper"
+                logiciel: "Reaper",
+                description : "",
+                cadre : "",
+                mention : ""
             },
             {
                 titre: "131_T_MICRO_chamberland_yannick",
                 type : "Audio",
-                logiciel : "Reaper"
+                logiciel : "Reaper",
+                description : "",
+                cadre : "",
+                mention : ""
             }
         ]
     }
