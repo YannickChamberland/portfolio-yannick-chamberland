2f09a0c5867871f01687c10e6d2d0a0b60109f69
YannickChamberland <142918103+YannickChamberland@users.noreply.github.com>
2025-10-21 11:36:07 -0400

    ajout du fetch et correction du mot projet dans le dossier json

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const app = Vue.createApp({
-data: function (){
-    return {
-
-    }
-}
-})
+    data() {
+      return {
+        projets: [] // tableau qui contiendra les données du JSON
+      }
+    },
+  
+   
+    mounted() {
+      fetch('projets.json') // fait le fetch dans le json projets
+        .then(response => response.json())
+        .then(data => {
+          this.projets = data
+        })
+        .catch(error => {
+          console.error('Erreur lors du chargement du JSON :', error)
+        })
+    },
+  
+    methods: {}
+  })
 
 
 app.mount('#app');
